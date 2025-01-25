d0b000930caba26bd16424915596d19a43d4cbaa
valdemir8086 <valdemirpraxedes92+github@gmail.com>
2025-01-25 04:02:37 -0300

    unindo consulta de livro com autores

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 
@@ -24,8 +25,15 @@ class LivroController {
 
     static async cadastrarLivro(req, res) {
         try {
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({message: "criado com sucesso", livro: novoLivro});
+            
+
+            const novoLivro = req.body;
+            const autorEncontrado = await autor.findById(novoLivro.autor)
+
+            const livroCompleto = { ...novoLivro, autor: { ... autorEncontrado._doc }}
+
+            const livroCriado = await livro.create(livroCompleto);
+            res.status(201).json({message: "criado com sucesso", livro: livroCriado});
         } catch(erro) {
             res.status(500).json({message: `${erro.message} - falha ao cadastrar livro`});
         }
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const livroSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true }, 
     titulo: { type: String, required: true, trim: true },
-    autor: { type: String, required: true, trim: true },
     genero: { type: String, required: true, trim: true },
     anoPublicacao: { type: Number, required: true, min: 0 },
     editora: { type: String, required: true, trim: true },
     disponivel: { type: Boolean, default: true },
     dataCadastro: { type: Date, default: Date.now },
     preco: { type: Number, required: true, min: 0 },
-    paginas: { type: Number, required: true, min: 1 }
+    paginas: { type: Number, required: true, min: 1 },
+    autor: autorSchema
+
   }, {versionKey: false});
 
   const livro = mongoose.model("livros", livroSchema);
