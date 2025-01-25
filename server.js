3185ad4ac8e74df5112575ff0538762516c25129
valdemir8086 <valdemirpraxedes92+github@gmail.com>
2025-01-24 21:42:35 -0300

    adicionado rota livros e autores

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Hello mundo!",
+    "/livros": "livros",
+    "/autores": "autores"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello mundo!");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
